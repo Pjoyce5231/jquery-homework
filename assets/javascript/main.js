970914362f51cb7fb649225778118932b216766c
pjoyce5231 <pjoyce5231@gmail.com>
2018-06-28 10:37:15 -0400

    last update

---
+++ assets/javascript/main.js
@@ -0,0 +1,14 @@
+//initiate the game (function)
+//on.click (function) that reads numbers on keyboard pressed
+
+
+//the code will generate a random number (probably from an array 1-12)
+//the user is given 4 "gems" to select from
+//each gem will be given a certain value 
+
+
+//if you lose the game, the retains your loses
+//if you win the game, the retains your wins
+
+//end game/reset(function)
+//each time the game resets, each gem will have a different value
